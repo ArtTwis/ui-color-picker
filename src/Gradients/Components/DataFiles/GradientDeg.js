6095d8b2289fd7b7605ebc7761205e0a34bfdf7d
Twinkle Goyal <twinkle.goyal@intimetec.com>
2022-11-22 02:16:00 +0530

    added GradientDeg.js

---
+++ src/Gradients/Components/DataFiles/GradientDeg.js
@@ -0,0 +1,56 @@
+export const GradientDeg = [
+  {
+    direction: 'to right',
+    deg: 0,
+    type: 'arrow',
+    func: 'linear-gradient',
+  },
+  {
+    direction: 'to right bottom',
+    deg: 45,
+    type: 'arrow',
+    func: 'linear-gradient',
+  },
+  {
+    direction: 'to bottom',
+    deg: 90,
+    type: 'arrow',
+    func: 'linear-gradient',
+  },
+  {
+    direction: 'to left bottom',
+    deg: 135,
+    type: 'arrow',
+    func: 'linear-gradient',
+  },
+  {
+    direction: 'to left',
+    deg: 180,
+    type: 'arrow',
+    func: 'linear-gradient',
+  },
+  {
+    direction: 'to left top',
+    deg: 225,
+    type: 'arrow',
+    func: 'linear-gradient',
+  },
+  {
+    direction: 'to top',
+    deg: 270,
+    type: 'arrow',
+    func: 'linear-gradient',
+  },
+  {
+    direction: 'to right top',
+    deg: 315,
+    type: 'arrow',
+    func: 'linear-gradient',
+  },
+  {
+    direction: 'circle',
+    deg: 0,
+    type: 'arrow',
+    func: 'radial-gradient',
+  },
+];
